1bf4e7f4b8b0b66c084f550a0642d0bfc620ed3d
Shadman Shafie <shadmanshafie@Shadmans-MacBook-Air.local>
2024-12-14 22:17:12 +0600

    Finally

---
+++ test3.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Use the C++ header for input/output
+
+int main() {
+    std::cout << "Hello" << std::endl; // Correct use of std namespace
+    return 0; // Return statement for the main function
+}
